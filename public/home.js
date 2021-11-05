ffea38937f915dccb78d09b12d0e1fac8d80aa86
Priyanka Gupta <86978649+PriyankaGeeta@users.noreply.github.com>
2021-11-05 13:03:38 +0530

    Update Home.js

--- public/home.js
+++ public/home.js
@@ -2,9 +2,9 @@ function Home() {
     
     return (
         <Card
-        bgcolor="primary"
-        header="BadBank International Bank"
-        title="Welcome to the BadBank"
+        bgcolor="info"
+        header="Bad Bank International Bank"
+        title="Welcome to the Bad Bank"
         text="You can move around using the navigation bar."
             body={(<img src="bank.png" className="img-fluid" alt="Responsive image"/>)}
         />
