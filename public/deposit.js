1b49fbca804a120b66667e5dbb2d3898e94254da
Priyanka Gupta <86978649+PriyankaGeeta@users.noreply.github.com>
2021-11-05 13:02:36 +0530

    Update deposit.js

--- public/deposit.js
+++ public/deposit.js
@@ -1,7 +1,9 @@
 function Deposit() {
     const ctx = React.useContext(UserContext); 
     const [status, setStatus]     = React.useState(true);
-    const [balance,setBalance] = React.useState(0)
+    const [balance,setBalance] = React.useState("");
+    const [enableButton, setEnableButton] = React.useState(false);
+    
     
     function fetchAccount() {
         if (ctx.user!=='') { 
@@ -9,12 +11,30 @@ function Deposit() {
         .then(response => response.json())
         .then(data => {
                 console.log(data);
-                setBalance('$' + data[0].balance);
+                setBalance('Your current balance is : $' + data[0].balance);
         });
         } 
     }
 
     fetchAccount();
+
+    function validateAmount(e){
+        if (e.currentTarget.value <= 0) {
+            setStatus("Amount cannot be negative");
+            setEnableButton(false)
+            setTimeout(() => setStatus(""), 1000);
+            
+        }
+        
+        else{
+        ctx.balance=e.currentTarget.value;
+        setEnableButton(true)
+        }
+       
+    }
+
+        
+    
 
     function depositAmount() {
         if (ctx.user!=='') { 
@@ -28,6 +48,7 @@ function Deposit() {
             var data = await res.json();
             console.log(data);
         })();
+         setEnableButton(false)
         } else {
             setStatus('Login to make a deposit');
             setTimeout(() => setStatus(''),3000);
@@ -36,21 +57,21 @@ function Deposit() {
 
     return (
         <Card
-            bgcolor="warning"
+            bgcolor="success"
             header="Deposit"
             text={balance}
             status={status}
             body={
                 <>
-                <CardForm
-                    showName="none"
-                    showPassword="none"
-                    showEmail="none"
-                />
-                {<button type="submit" className="btn btn-light" onClick={depositAmount}>Deposit</button>}
+                Amount
+                <br/>
+                <input type="number" className="form-control" placeholder="Enter amount" onChange={e => validateAmount(e) }/>
+                <br/>
+                {<button type="submit" className="btn btn-light" onClick={depositAmount} disabled={!enableButton}>Deposit</button>}
                 </>
             }
         />
     )
 }
+
 
