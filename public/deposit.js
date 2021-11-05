8387572222922afb2a4ebc65544b34bd9a7d4684
Priyanka Gupta <86978649+PriyankaGeeta@users.noreply.github.com>
2021-11-05 21:23:43 +0530

    Update deposit.js

--- public/deposit.js
+++ public/deposit.js
@@ -19,13 +19,19 @@ function Deposit() {
     fetchAccount();
 
     function validateAmount(e){
-        if (e.currentTarget.value <= 0) {
-            setStatus("Amount cannot be negative");
+        if (isNaN(+e.currentTarget.value)){
+            setStatus("ERROR : NAN! Amount entered should be a Number");
             setEnableButton(false)
-            setTimeout(() => setStatus(""), 1000);
+            setTimeout(() => setStatus(""), 3000);
+            return;
+        }
+
+        if (e.currentTarget.value <= 0) {
+            setStatus("ERROR : Amount cannot be negative");
+            setEnableButton(false)
+            setTimeout(() => setStatus(""), 3000);
             
         }
-        
         else{
         ctx.balance=e.currentTarget.value;
         setEnableButton(true)
@@ -39,7 +45,7 @@ function Deposit() {
     function depositAmount() {
         if (ctx.user!=='') { 
         setStatus(`$${ctx.balance} deposit successful!`);
-        setTimeout(() => setStatus(''),2000);
+        setTimeout(() => setStatus(''),3000);
 
         ctx.balance.toString();
         const url = `/account/deposit/${ctx.user}/${ctx.balance}`;
@@ -65,7 +71,7 @@ function Deposit() {
                 <>
                 Amount
                 <br/>
-                <input type="number" className="form-control" placeholder="Enter amount" onChange={e => validateAmount(e) }/>
+                <input  className="form-control" placeholder="Enter amount" onChange={e => validateAmount(e) }/>
                 <br/>
                 {<button type="submit" className="btn btn-light" onClick={depositAmount} disabled={!enableButton}>Deposit</button>}
                 </>
@@ -73,5 +79,3 @@ function Deposit() {
         />
     )
 }
-
-
